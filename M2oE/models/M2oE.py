1cffe797e07c09fde9228de940bd0d08850aed4f
LIU <ryuuchou17@gmail.com>
2025-07-28 17:50:10 +0900

    Update M2oE

--- M2oE/models/M2oE.py
+++ M2oE/models/M2oE.py
@@ -2,6 +2,99 @@ import torch
 import torch.nn as nn
 from torch.nn.modules.linear import Linear
 
+class GraphAttention(nn.Module):
+    def __init__(self, num_nodes, parent_map):
+        super(GraphAttention, self).__init__()
+        self.parent_map = parent_map.to(self.device)
+        self.offset = offset.to(self.device)
+        self.num_nodes = args.joint_num
+
+        # init 
+        self.spatial_encoding_raw = self.init_para().to(torch.int).to(self.device)
+
+        # parameters
+        self.degree_embedding = nn.Embedding(self.num_degree, self.args.d_model, padding_idx=0)
+        self.degree_embedding = self.degree_embedding.to(self.device)
+        num_spatial_encoding = len(set(self.spatial_encoding_raw.reshape(-1).tolist()))
+        self.spatial_embedding = nn.Embedding(num_spatial_encoding,  self.args.n_head, padding_idx=0)
+        self.spatial_embedding = self.spatial_embedding.to(self.device)
+
+    def init_para(self):
+        with torch.no_grad():
+            self.degree, self.adjacency = self.compute_degree_and_adjancency()
+            self.degree = self.degree.to(torch.int).to(self.device)
+            self.num_degree = int(torch.max(self.degree).item()) + 1
+
+            self.adjacency = self.adjacency.to(self.device)
+            self.SPD = djikstra_all_pairs(self.adjacency)
+            spatial_encoding_raw = torch.zeros( self.parent_map.shape[0],  self.parent_map.shape[0])
+            for i in range( self.parent_map.shape[0]):
+                for j in range( self.parent_map.shape[0]):
+                    if i == j:
+                        continue
+                    spatial_encoding_raw[i, j] = self.SPD[i][j][1]
+
+            max_weight_num = 0
+            for start in range(self.num_nodes):
+                for end in range(start):
+                    max_weight_num = max(max_weight_num, self.SPD[start][end][1])
+
+            self.offset_weight = nn.Parameter(torch.zeros(self.num_nodes, self.joint_num, self.args.n_head, max_weight_num * 3)).to(self.device)
+            self.offset_bias = nn.Parameter(torch.zeros(self.num_nodes, self.joint_num, self.args.n_head, 1)).to(self.device)
+
+        return spatial_encoding_raw
+
+    def forward(self):
+        # degree encoding
+        degree_encoding = self.degree_embedding(self.degree)
+        degree_encoding.to(self.device)
+
+        # spatial encoding
+        spatial_encoding = self.spatial_embedding(self.spatial_encoding_raw)
+        spatial_encoding = spatial_encoding.permute(2, 0, 1)
+        spatial_encoding.to(self.device)
+
+        # offset encoding
+        offset_encoding = torch.zeros(self.num_nodes, self.joint_num, self.args.n_head).to(self.device)
+        for start in range(self.num_nodes):
+            for end in range(self.num_nodes):
+                if start == end:
+                    continue
+                weight_num = self.SPD[start][end][1]
+                path = self.SPD[start][end][0]
+                
+                weight = self.offset_weight[start, end, :, :weight_num * 3]
+                bias = self.offset_bias[start, end, :, 0]
+                path_feature = torch.cat([self.offset[path[i], path[i+1]] for i in range(len(path) - 1)], dim=0)
+                
+                offset_encoding[start, end] = torch.matmul(weight, path_feature) + bias / weight_num
+
+        offset_encoding = offset_encoding.permute(2, 0, 1)
+        return degree_encoding, spatial_encoding, offset_encoding
+
+        
+    def compute_degree_and_adjancency(self):
+        degree = torch.zeros(self.args.joint_num)
+        adjacency = torch.zeros(self.args.joint_num, self.args.joint_num)
+
+        for i in range(self.args.joint_num):
+            if self.parent_map[i] == -1:
+                continue
+            degree[i] += 1
+            degree[self.parent_map[i]] += 1
+            adjacency[i, self.parent_map[i]] = 1
+            adjacency[self.parent_map[i], i] = 1
+
+        return degree, adjacency
+    
+
+# class M2oEGate(nn.Module):
+    # def __init__(self, num_obs, max_num_modulars, embedding_dim, num_heads):
+        # super().__init__()
+        # self.embedding = 
+
+
+
 
 class M2oE(nn.Module):
     def __init__(self, num_obs, num_global_obs, hidden_dim, max_num_modules,

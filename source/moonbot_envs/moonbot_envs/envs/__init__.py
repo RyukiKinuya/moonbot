2ea449b9b541e0b6356603f30d5e8af0ccd605bd
LIU <ryuuchou17@gmail.com>
2025-08-25 07:53:31 +0000

    Upload image.

--- source/moonbot_envs/moonbot_envs/envs/__init__.py
+++ source/moonbot_envs/moonbot_envs/envs/__init__.py
@@ -13,8 +13,8 @@ gym.register(
     kwargs={
         "env_cfg_entry_point": IntegrationEnvCfg,
         "M2oE_entry_point": agents.M2oE_cfg.M2oE_Cfg
-        # "M2oE_entry_point": agents.Transformer_cfg.Transformer_Cfg
-        # "M2oE_entry_point": agents.SharedMLP_cfg.SharedMLP_Cfg
+        #"M2oE_entry_point": agents.Transformer_cfg.Transformer_Cfg
+        #"M2oE_entry_point": agents.SharedMLP_cfg.SharedMLP_Cfg
         # "M2oE_entry_point": agents.JointMLP_cfg.JointMLP_Cfg
     }
 )

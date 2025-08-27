1c156b0953c97f56cdd5706ee532ba924f4fd1cd
LIU <ryuuchou17@gmail.com>
2025-08-27 06:24:00 +0000

    Upload Image.

--- source/moonbot_envs/moonbot_envs/envs/agents/M2oE_cfg.py
+++ source/moonbot_envs/moonbot_envs/envs/agents/M2oE_cfg.py
@@ -23,9 +23,9 @@ class M2oE_Cfg(M2oEOnPolicyRunnerCfg):
         gate_type="attention",
         gate_embedding_dim=32,
         gate_num_heads=4,
-        gate_num_layers=1,
+        gate_num_layers=2,
         gate_dropout=0.1,
-        gate_use_positional_embedding=False,
+        gate_use_positional_embedding=True,
     )
     algorithm = RslRlPpoAlgorithmCfg(
         value_loss_coef=1.0,

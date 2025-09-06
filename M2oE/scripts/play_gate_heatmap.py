408ba1f9a097b6a0230678c2e973600d75369300
LIU <ryuuchou17@gmail.com>
2025-09-06 06:54:55 +0000

    Upload heatmap.

--- M2oE/scripts/play_gate_heatmap.py
+++ M2oE/scripts/play_gate_heatmap.py
@@ -206,7 +206,7 @@ def main():
         figsize=(0.5 * gate_matrix.shape[1] + 5, 0.5 * num_experts + 2)
     )
 
-    im = ax.imshow(gate_matrix, vmin=0, vmax=1, cmap="YlGn", aspect="auto")
+    im = ax.imshow(gate_matrix, vmin=0, vmax=0.3, cmap="YlGn", aspect="auto")
 
     for sp in ax.spines.values():
         sp.set_visible(False)

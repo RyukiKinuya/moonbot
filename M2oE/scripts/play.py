0170b7d4a6752399fa1cdac7f2541eee7e7375a3
LIU <ryuuchou17@gmail.com>
2025-09-06 06:34:32 +0000

    Upload video.

--- M2oE/scripts/play.py
+++ M2oE/scripts/play.py
@@ -15,7 +15,7 @@ parser.add_argument(
     "--disable_fabric", action="store_true", default=False, help="Disable fabric and use USD I/O operations."
 )
 parser.add_argument("--num_envs", type=int, default=1, help="Number of environments to simulate.")
-parser.add_argument("--task", type=str, default="Integration_Locomotion_Play_v1", help="Name of the task.")
+parser.add_argument("--task", type=str, default="Integration_Locomotion_v1", help="Name of the task.")
 parser.add_argument("--real_time", action="store_true", default=False, help="Run in real-time, if possible.")
 
 cli_args.add_m2oe_rl_args(parser)

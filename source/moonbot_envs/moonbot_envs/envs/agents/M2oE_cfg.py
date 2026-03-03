f95c24af9c54ec8134ca7d8be63ba1b123105f4e
LIU <ryuuchou17@gmail.com>
2026-03-03 12:35:06 +0000

    Upload image w/o lb

--- source/moonbot_envs/moonbot_envs/envs/agents/M2oE_cfg.py
+++ source/moonbot_envs/moonbot_envs/envs/agents/M2oE_cfg.py
@@ -33,7 +33,7 @@ class M2oE_Cfg(M2oEOnPolicyRunnerCfg):
         use_clipped_value_loss=True,
         clip_param=0.2,
         entropy_coef=0.001,
-        load_balance_loss_coef=0.1,
+        load_balance_loss_coef=0.0,
         num_learning_epochs=5,
         num_mini_batches=4,
         learning_rate=1.0e-3,
